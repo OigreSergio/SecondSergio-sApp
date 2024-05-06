1a3f2e771d30da5a30663c174597a92c2935dfed
Sergio <sergio.hanganu@gmail.com>
2024-05-06 10:11:37 +0200

    Second commit

--- src/main/java/com/example/SergiosAppdemo/AppClass.java
+++ src/main/java/com/example/SergiosAppdemo/AppClass.java
@@ -1,4 +1,5 @@
 package com.example.SergiosAppdemo;
+
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -7,7 +8,7 @@ public class AppClass {
     @GetMapping(path = "/ciao/Lombardia")
     public parameterRiquest requestParam(
             @RequestParam(defaultValue = "Lombardia") String provincia
-            ,@RequestParam(defaultValue = "Giuseppe") String nome) {
+            , @RequestParam(defaultValue = "Giuseppe") String nome) {
         return new parameterRiquest(nome, provincia);
     }
 
--- src/main/java/com/example/SergiosAppdemo/SergiosAppdemoApplication.java
+++ src/main/java/com/example/SergiosAppdemo/SergiosAppdemoApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SergiosAppdemoApplication {
+public class DemoApp {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SergiosAppdemoApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApp.class, args);
+    }
 
 }
