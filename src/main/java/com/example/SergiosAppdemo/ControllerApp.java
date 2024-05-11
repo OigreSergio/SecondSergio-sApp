640b6719b2790192142c4c0aa56b7642aeaf033b
Sergio <sergio.hanganu@gmail.com>
2024-05-11 18:51:33 +0200

    Correction made

--- src/main/java/com/example/SergiosAppdemo/ControllerApp.java
+++ src/main/java/com/example/SergiosAppdemo/ControllerApp.java
@@ -4,11 +4,9 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/v2")
-public class AppClass {
+public class ControllerApp {
     @GetMapping(path = "/ciao/Lombardia")
-    public parameterRiquest requestParam(
-            @RequestParam(defaultValue = "Lombardia") String provincia
-            , @RequestParam(defaultValue = "Giuseppe") String nome) {
+    public parameterRiquest requestParam(@RequestParam(defaultValue = "Lombardia") String provincia, @RequestParam(defaultValue = "Giuseppe") String nome) {
         return new parameterRiquest(nome, provincia);
     }
 
